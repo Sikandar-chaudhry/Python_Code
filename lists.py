b861aa9a4f9ff9da50f5a99305d98cc622f1a102
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-06 18:39:18 +0500

    More files

---
+++ lists.py
@@ -0,0 +1,12 @@
+#Define a list of names
+names =["Sikander","Hamza","Asim","Qasim"]
+
+print(names)
+print(names[1])
+
+#List is mutable , I can change
+names.append("AbdulMuqeet")
+
+#Sorting the elements
+names.sort()
+print(names)
