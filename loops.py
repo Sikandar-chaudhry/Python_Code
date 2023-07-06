26285cb3906f7abd9a5073a17988474580279d1a
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-06 19:04:32 +0500

    Add loops.py

---
+++ loops.py
@@ -0,0 +1,14 @@
+for i in [0,1,2,3,4,5]:
+    print(i)
+    
+#Using range method
+for i in range(10):
+    print(i)
+    
+names =["Sikander","Hamza","Asim","Qasim","Mohsin"]
+for name in names:
+    print(name)
+    
+name = "Sikander"
+for character in name:
+    print(character)
