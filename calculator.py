6bfecba8a967e4b12525d5330f65596f3fc5fa73
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-11 19:10:22 +0500

    Add better code for command line interaction

--- calculator.py
+++ calculator.py
@@ -16,26 +16,6 @@ def modulus(a,b):
 control = True
 while control:
     
-    try:
-        first_number = input("\nFirst Number : ")
-        if '.' in first_number:
-            first_number = float(first_number)
-        else:
-            first_number = int(first_number)
-    except ValueError:
-        print("Error: Invalid Input")
-        sys.exit(1)
-    
-    try:
-        second_number = input("Second Number : ")
-        if '.' in second_number:
-            second_number = float(second_number)
-        else:
-            second_number = int (second_number)
-    except ValueError:
-        print("Error: Invalid Input")
-        sys.exit(1)
-    
     print("\n1. Addition")
     print("2. Subtraction")
     print("3. Multiplication")
@@ -46,24 +26,131 @@ while control:
     
     #Cases
     if choice==1:
+        
+        try:
+            first_number = input("\nFirst Number : ")
+            if '.' in first_number:
+                first_number = float(first_number)
+            else:
+                first_number = int(first_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+            
+        try:
+            second_number = input("Second Number : ")
+            if '.' in second_number:
+                second_number = float(second_number)
+            else:
+                second_number = int (second_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+        
         print(addition(first_number,second_number))
         print("\n")
     if choice==2:
+        
+        try:
+            first_number = input("\nFirst Number : ")
+            if '.' in first_number:
+                first_number = float(first_number)
+            else:
+                first_number = int(first_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+            
+        try:
+            second_number = input("Second Number : ")
+            if '.' in second_number:
+                second_number = float(second_number)
+            else:
+                second_number = int (second_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+        
         print(subtraction(first_number,second_number))
         print("\n")
     if choice==3:
+        
+        try:
+            first_number = input("\nFirst Number : ")
+            if '.' in first_number:
+                first_number = float(first_number)
+            else:
+                first_number = int(first_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+            
+        try:
+            second_number = input("Second Number : ")
+            if '.' in second_number:
+                second_number = float(second_number)
+            else:
+                second_number = int (second_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+        
         print(multiplication(first_number,second_number))
         print("\n")
     if choice==4:
+        
+        try:
+            first_number = input("\nFirst Number : ")
+            if '.' in first_number:
+                first_number = float(first_number)
+            else:
+                first_number = int(first_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+            
+        try:
+            second_number = input("Second Number : ")
+            if '.' in second_number:
+                second_number = float(second_number)
+            else:
+                second_number = int (second_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+        
         try:
             print(division(first_number,second_number))
         except ZeroDivisionError:
             print("Cannot divide by zero")  
             #No sys.exit(1) for division , let user do other mathematical operations or do division after validating input values      
         print("\n")
+   
     if choice==5:
+        
+        try:
+            first_number = input("\nFirst Number : ")
+            if '.' in first_number:
+                first_number = float(first_number)
+            else:
+                first_number = int(first_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+            
+        try:
+            second_number = input("Second Number : ")
+            if '.' in second_number:
+                second_number = float(second_number)
+            else:
+                second_number = int (second_number)
+        except ValueError:
+            print("Error: Invalid Input")
+            sys.exit(1)
+        
         print(modulus(first_number,second_number))
         print("\n")
+   
     if choice==6:
         control = False
         print("Quiting the program\n")
